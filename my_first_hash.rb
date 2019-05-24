2e2a91072f53a362691ddfcf5246dd6e0d4d4387
Sam Markham <sam.e.markham@gmail.com>
2019-05-24 18:55:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
-def my_hash
+def my_hash 
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = { 
+   "Supergirl" => "Kara Danvers", 
+   "Martian Manhunter" => "J'onn J'onzz"
+   "Miss Martian" => "M'gann M'orzz"
+  }
   
 end
 
